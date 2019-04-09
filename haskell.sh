c9997528308b0958069ccbf0a0fcb201df44741a
Artur Czajka <arturczajka@gmail.com>
2019-04-09 14:52:57 +0000

    Use stack

--- haskell.sh
+++ haskell.sh
@@ -1,24 +1,13 @@
 #!/bin/bash -ueE
 
-sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get update -qq
-sudo apt-get install -qq ghc-7.10.3 cabal-install-1.22 git gvfs-bin
+curl -sSL https://get.haskellstack.org/ | sh
 
-sudo ln -s /opt/ghc/7.10.3/bin/* /usr/local/bin/
-sudo ln -s /opt/cabal/1.22/bin/* /usr/local/bin/
-
-if [[ $PATH != */usr/local/bin/* ]]; then
-    export PATH="/usr/local/bin/:$PATH"
-fi
-
-cabal update
-cabal install happy hlint stylish-haskell ghc-mod
-
-sudo ln -s ~/.cabal/bin/* /usr/local/bin/
+stack update
+stack install hlint stylish-haskell ghc-mod
 
 wget -O atom.deb https://atom.io/download/deb
 sudo dpkg -i atom.deb
 rm atom.deb
 apm update
-apm install autocomplete-haskell haskell-ghc-mod ide-haskell ide-haskell-cabal language-haskell
-apm install atom-material-syntax atom-material-ui minimap
+apm install autocomplete-haskell haskell-ghc-mod ide-haskell ide-haskell-hoogle language-haskell
+apm install minimap
